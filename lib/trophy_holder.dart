c88fe5cf1ccc66988bb5172be4f5dd972c0c8946
Mateusz Wojdyła <mateusz.wojdyla@asseco.pl>
2024-10-24 16:54:17 +0200

    Conditional color filtering of trophy image

--- lib/trophy_holder.dart
+++ lib/trophy_holder.dart
@@ -33,11 +33,26 @@ class _TrophyHolderState extends State<TrophyHolder> {
         ? 'You own the trophy! You are the best!'
         : 'Don\'t give up! Next time you\'ll win!';
 
+    final conditionalColorFilter = holdingTrophy
+        ? const ColorFilter.mode(
+            Colors.transparent,
+            BlendMode.multiply,
+          )
+        : const ColorFilter.matrix(<double>[
+            0.2126, 0.7152, 0.0722, 0, 0, // Red channel
+            0.2126, 0.7152, 0.0722, 0, 0, // Green channel
+            0.2126, 0.7152, 0.0722, 0, 0, // Blue channel
+            0, 0, 0, 1, 0, // Alpha channel (preserve transparency)
+          ]);
+
     return MaterialApp(
       home: Scaffold(
         body: Column(
           children: [
-            Image.asset('assets/images/trophy2.png'),
+            ColorFiltered(
+              colorFilter: conditionalColorFilter,
+              child: Image.asset('assets/images/trophy2.png'),
+            ),
             const SizedBox(height: 20),
             Text(motivationalText),
             const SizedBox(height: 250),
